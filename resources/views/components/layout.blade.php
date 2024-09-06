500c3941435bafbe5edf97f45e362a3092f822c0
“OmarAsem27” <omarasem6215@gmail.com>
2024-09-06 11:31:20 +0300

    built components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @vite(['resources/js/app.js','resources/css/app.css'])
+</head>
+
+<body class="bg-black">
+    <div class="px-10 text-white">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div><a href="#" >Post a job</a></div>
+        </nav>
+        <main class="mt-10 mx-auto max-w-[986px]">
+
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,2 @@
+<a href="#"
+    class="bg-white/10 hover:bg-white/25 text-xs px-2 py-1 rounded-xl transitions-colors duration-300">{{ $slot }}</a>
